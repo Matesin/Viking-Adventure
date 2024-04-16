a30ef38fd6c0caea249ad8ca4955b5959c0e6181
Matyáš Martan <martama1@fel.cvut.cz>
2024-04-16 08:27:07 +0200

    edited MapManager

--- src/map/MapManager.java
+++ src/map/MapManager.java
@@ -1,5 +1,6 @@
 package map;
 
+import com.sun.media.jfxmedia.events.PlayerEvent;
 import entity.Player;
 import gameloop.GamePanel;
 import javafx.scene.canvas.GraphicsContext;
@@ -16,8 +17,6 @@ public class MapManager {
     private int mapWidth;
     @Getter
     private int mapHeight;
-    public int tileSize;
-    public int mapIndex;
     GamePanel gamePanel;
     public MapManager(GamePanel gamePanel) {
         this.gamePanel = gamePanel;
@@ -33,14 +32,14 @@ public class MapManager {
         int screenY = gamePanel.player.screenCoordY;
         int offsetX = screenX - playerX;
         int offsetY = screenY - playerY;
-        int startCol = Math.max(playerX / tileSize, 0);
-        int startRow = Math.max(playerY / tileSize, 0);
-        int endCol = Math.min(startCol + gamePanel.SCREEN_COLS, mapWidth);
-        int endRow = Math.min(startRow + gamePanel.SCREEN_ROWS, mapHeight);
+        int startCol = Math.max(playerX / GamePanel.TILE_SIZE, 0);
+        int startRow = Math.max(playerY / GamePanel.TILE_SIZE, 0);
+        int endCol = Math.min(startCol + GamePanel.SCREEN_COLS, mapWidth);
+        int endRow = Math.min(startRow + GamePanel.SCREEN_ROWS, mapHeight);
         for (int row = startRow; row < endRow; row++) {
             for (int col = startCol; col < endCol; col++) {
                 int tile = this.map.getMap()[row][col];
-                gc.drawImage(map.tiles[tile].image, (col - startCol) * tileSize + offsetX, (row - startRow) * tileSize + offsetY);
+                gc.drawImage(map.tiles[tile].image, (col - startCol) * GamePanel.TILE_SIZE + offsetX, (row - startRow) * GamePanel.TILE_SIZE + offsetY);
             }
         }
     }
